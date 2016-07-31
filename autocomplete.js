38edc019e7f725b710f06ba2c370800993ef6cca
Colin Zarnegar <c.zarnegar@gmail.com>
2016-07-31 13:44:50 -0700

    adds error handling to autocomplete.js and removes old code

--- autocomplete.js
+++ autocomplete.js
@@ -30,50 +30,27 @@ function autoComplete(opts, cb) {
 	  input: readStream
 	});
 
-	rl.on('line', (line) => {
-		line = line.trim();
-	  var words = line.split(' ');
-	  while(words.length > 0){
-	  	var word = words.pop();
-	  	word = word.replace(trailingSymbol,'');
-	  	word = word.toLowerCase();
-	  	if(!validWord.test(word)){
-	  		suggestTree.add(word);	  		
-	  	}
-	  }
-	});
+	try {
+		rl.on('line', (line) => {
+			line = line.trim();
+		  var words = line.split(' ');
+		  while(words.length > 0){
+		  	var word = words.pop();
+		  	word = word.replace(trailingSymbol,'');
+		  	word = word.toLowerCase();
+		  	if(!validWord.test(word)){
+		  		suggestTree.add(word);	  		
+		  	}
+		  }
+		});
 
-	rl.on('close', () => {
-		cb(null, suggestTree);
-	});
-
-	// console.log('Starting to add words in trie');
-	// readStream.on('data', function(data) {
-	// 	remaining += data;
-	// 	var index = remaining.indexOf(' ');
-	// 	var last  = 0;
-	// 	while (index > -1) {
-	// 	  var line = remaining.substring(last, index).trim().toLowerCase().replace(/[\']/g,'');
-	// 	  last = index + 1;
-	// 	  var notValid = validWord.test(line);
-	// 	  if(!notValid && line.length > 0){
-	// 	  	line = line.replace(trailingSymbol,'')
-	// 	  	// dictionary[line] = dictionary[line] ? dictionary[line]++ : 1;
-	// 	  	suggestTree.add(line);
-	// 	  }
-	// 	  index = remaining.indexOf(' ', last);
-	// 	}
-	// 	remaining = remaining.substring(last).trim().toLowerCase();
-	// });
-
-	// readStream.on('end', function() {
-	// 	var notValid = validWord.test(remaining);
-	//   if (!notValid && remaining.length > 0) {
-	//   	remaining = remaining.replace(trailingSymbol,'')
-	//     suggestTree.add(remaining);
-	//   }
-	// 	cb(null, suggestTree);
-	// });
+		rl.on('close', () => {
+			cb(null, suggestTree);
+		});
+	}
+	catch (err) {
+		cb(err, null);
+	}
 }
 
 module.exports = autoComplete;
